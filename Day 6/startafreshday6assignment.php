bd296c98d59e0dd65da570c02c50b14187765758
Mayorkun27 <156526127+Mayorkun27@users.noreply.github.com>
2024-07-08 11:51:58 +0100

    Added implementation of Day 6 assignment, including functions for checking even/odd numbers, calculating sums....Just to read 3, 4 and 5

--- Day 6/startafreshday6assignment.php
+++ Day 6/startafreshday6assignment.php
@@ -1,3 +1,53 @@
 <?php
-    // ASSIGNMENT --- Write a function to check if a function is even or odd
+$break = "<br>";
+    // ASSIGNMENT --- Write a function to check if a function is even or odd 2.Calculate the sum of 1 to 10 3.Read about functions returning an array 4.Read about recursive functions 5.Read about Global function
+
+    //1. Check if a function is even or odd
+    function checkEvenOdd($number) {
+        // Check if there is a remainder after divide by 2
+        if ($number % 2 == 0) {
+            return "Number ".$number." is Even";
+        } else {
+            return "Number ".$number." is Odd";
+        }
+    }
+
+    echo(checkEvenOdd(3));
+    echo $break;
+
+    //2. Calculate the sum of 1 to 10
+    function sumFunction($start, $stop) {
+        $sum = 0;
+        for ($i = $start; $i <= $stop; $i++) {
+            $sum += $i;
+        }
+        return "The sum of values between" . $start . " to " . $stop . " is: " . $sum;
+    }
+    echo sumFunction(2, 7);
+
+    // Read about functions returning an array
+    // function arrayFunction() {
+    //     return array(1, 2, 3, 4, 5);
+    // }
+    // $array = arrayFunction();
+    // echo "The array is: ".implode(", ", $array);
+
+    // Read about recursive functions
+    // function recursiveFunction($n) {
+    //     if ($n > 0) {
+    //         echo $n.", ";
+    //         recursiveFunction($n - 1);
+    //     }
+    // }
+    // recursiveFunction(10);
+
+    // Read about Global function
+    // $globalVariable = "This is a global variable";
+    // function globalFunction() {
+    //     global $globalVariable;
+    //     echo "This is a global function: ".$globalVariable;
+    // }
+    // globalFunction();
+
+    
 ?>
