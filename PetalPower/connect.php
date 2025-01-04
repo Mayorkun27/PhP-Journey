d8e8eda6af049fab3634447701c35ca480371704
Mayorkun27 <156526127+Mayorkun27@users.noreply.github.com>
2025-01-04 15:32:49 +0100

    Updated connect.php in petalpower api

--- PetalPower/connect.php
+++ PetalPower/connect.php
@@ -1,15 +1,16 @@
 <?php
 
-    header("Access-Control-Allow-Origin: *");
+    header('Access-Control-Allow-Origin: http://localhost:5173');
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
     header('Content-Type: application/json');
-    
+
     $conn = mysqli_connect("localhost", "root", "", "petal_power");
 
     if ($conn) {
-        echo "connected";
+        // echo "connected";
         echo json_encode(["status" => 200, "message" => "Database connection successful"]);
     } else {
-        echo "Not connected";
+        // echo "Not connected";
         echo json_encode(["status" => 500, "message" => "Database connection failed"]);
     };
 
